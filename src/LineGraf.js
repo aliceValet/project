962d8c9baa93883b2f7e1d68cb87b77eaebc6517
Alice VALET <alice_valet@orange.fr>
2019-04-05 23:53:15 +0200

    Linegraph is linked to the backend. in total 4 out of 6 are linked to the backend

--- src/LineGraf.js
+++ src/LineGraf.js
@@ -2,41 +2,31 @@ import React, { PureComponent } from 'react';
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
-
-const data = [
-  {
-    name: '00h', uv: 0, pv: 0,
-  },
-  {
-    name: '3h', uv: 0, pv: 0,
-  },
-  {
-    name: '6h', uv: 30, pv: 50,
-  },
-  {
-    name: '9h', uv: 10, pv: 20, 
-  },
-  {
-    name: '12h', uv: 0, pv: 0, 
-  },
-  {
-    name: '15h', uv: 0, pv: 0, 
-  },
-  {
-    name: '18h', uv: 0, pv: 50, 
-  },
-  {
-    name: '21h', uv: 40, pv: 100, 
-  },
-  {
-    name: '00h', uv: 20, pv: 50, 
-  },
-];
+import axios from 'axios';
+import _ from 'lodash';
 
 class LineGraph extends PureComponent {
   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
+   state = {
+    tracksList: []
+  }
+
+
+  componentDidMount(){
+     axios.get(`http://localhost:3000/tracks/`)
+      .then(res => {
+        console.log("res.dat", res.data);
+          this.setState({ tracksList: res.data });
+        });
+
+  }
 
   render() {
+    const values = [];
+
+    {_.map(this.state.tracksList, o =>
+      values.push({name : o.Duration, pv : o.Listenings}) )}
+    console.log(values)
     return ( 
       <div style={{ width: '100%', height: 300, margin : 20}}>
         <ResponsiveContainer>
@@ -44,7 +34,7 @@ class LineGraph extends PureComponent {
       <LineChart
         width={1000}
         height={500}
-        data={data}
+        data={values}
         margin={{
           top: 0, right: 50, left: 50, bottom: 50,
         }}
@@ -54,8 +44,7 @@ class LineGraph extends PureComponent {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line name="Winter" type="monotone" dataKey="pv" stroke="#B0008E" activeDot={{ r: 8 }} />
-        <Line name="Summer" type="monotone" dataKey="uv" stroke="#FFCD00" />
+        <Line name="Nb of Listenings according to the Duration" type="monotone" dataKey="pv" stroke="#B0008E" activeDot={{ r: 8 }} />
       </LineChart>
       </ResponsiveContainer>
       </div>
--- src/TableComp.js
+++ src/TableComp.js
@@ -2,53 +2,49 @@ import React from 'react';
 import './TableComp.css';
 import { Table } from 'reactstrap';
 import axios from 'axios';
+import _ from 'lodash'
 
 
 class TableCompo extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      Name : "",
-      Birthday:0,
-      Followers:0
-    }
+  state = {
+    artistList: []
+  }
+
+  componentDidMount(){
+     axios.get(`http://localhost:3000/artists/`)
+      .then(res => {
+        console.log("res.dat", res.data);
+          this.setState({ artistList: res.data });
+        });
 
   }
-  componentDidMount(){
-    axios.get('http://localhost:3000/artists/')
-     .then(res => {
-        const artists = res.data;
-        this.setState({ artists });
-      })
-    .catch(error =>{
-        console.log(error);
-      });
-  }
+  
   render() {
-    const artists = this.state;
     return (
-    	 <Table hover style={{width:'100%'}}>
+       <Table hover style={{width:'100%'}}>
         <thead>
           <tr>
-            <th>Artists</th>
+            <th>Trending Artists</th>
+            <th>Name</th>
             <th>Birthday</th>
             <th>Followers</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">NF</th>
-            <td>1980</td>
-            <td>2000</td>
+        {_.map(this.state.artistList, o =>
+          
+            <tr>
+            <th scope="row">{_.uniqueId()}</th>
+            <td>{o.Name}</td>
+            <td>{o.Birthday}</td>
+            <td>{o.Followers}</td>
           </tr>
-          <tr>
-            <th scope="row">MJ</th>
-            <td>1959</td>
-            <td>3000</td>
-          </tr>
-        </tbody>
+          
+        )}
+          
+                </tbody>
       </Table>
-    	
+      
     );
   }
 }
